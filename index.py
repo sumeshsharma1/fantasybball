798a65260d93f1e30d2defd7067a782658250d8d
sumeshsharma1 <35505694+sumeshsharma1@users.noreply.github.com>
2019-12-17 15:51:03 -0800

    import server

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from apps import playerTable, salaryCalculator
 
 app.layout = html.Div([
